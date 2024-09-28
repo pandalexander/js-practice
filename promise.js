5125c48b345ee6080e4f7765220efb8131721215
Alex Seeley <alexseeley134@gmail.com>
2024-09-28 15:45:05 -0600

    Add first promise to promise practice

---
+++ promise.js
@@ -0,0 +1,4 @@
+const result = fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json))
+  .catch((err) => console.log(err));
