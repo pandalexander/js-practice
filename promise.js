85b0726d8915a26f13613b90c8c0ba4d3ec6b3ef
Alex Seeley <alexseeley134@gmail.com>
2024-10-01 22:37:55 -0600

    Complete promise challenge 1

--- promise.js
+++ promise.js
@@ -1,4 +1,18 @@
-const result = fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
-  .then((json) => console.log(json))
-  .catch((err) => console.log(err));
+// Challenge 1: Basic Promise
+// Create a function called fetchData that returns a promise which resolves to a string "Data received!" after a delay of 2 seconds. Use setTimeout to simulate the delay.
+
+function fetchData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Data received!");
+    }, 2000);
+  });
+}
+
+// fetchData().then((data) => {
+//   console.log(data);
+// });
+
+fetchData().then((data) => {
+  console.log(data);
+});
